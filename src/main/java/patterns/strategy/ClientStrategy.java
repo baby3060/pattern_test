9e1e5fae9509156dd86530ebc0abf4e9b84bda73
kkk <kkk@gmail.com>
2019-03-02 16:03:51 +0900

    전략 패턴 적용 파일 최종수정

--- src/main/java/patterns/strategy/ClientStrategy.java
+++ src/main/java/patterns/strategy/ClientStrategy.java
@@ -1,9 +1,10 @@
 package patterns.strategy;
 
-import patterns.strategy.apply_before.*;
+import patterns.strategy.apply_after.*;
 
 public class ClientStrategy {
     public void run() {
+        /*
         Boss boss = new FlyBoss();
         
         boss.move();
@@ -11,5 +12,26 @@ public class ClientStrategy {
         boss = new GentleBoss();
 
         boss.attack();
+        */
+
+        Boss boss = new GentleBoss();
+        performTemplate(boss);
+        
+        System.out.println();
+
+        boss = new FlyBoss();
+        performTemplate(boss);
+
+        System.out.println();
+
+        boss = new GroundBoss();
+        performTemplate(boss);
+    }
+
+    private void performTemplate(Boss boss) {
+        boss.show();
+        boss.performAttack();
+        boss.performAvoid();
+        boss.performMove();
     }
 }
