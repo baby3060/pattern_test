3dc92a9b22deaa34fb9545af0f1e8e8afd2e9338
kkk <kkk@gmail.com>
2019-03-02 13:55:18 +0900

    전략 패턴 용 Main 클래스 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,13 @@
  * 항상 실행되는 클래스
  * 각 패턴 패키지의 Client[Pattern명] 클래스를 수행한다.
  */
+
+import patterns.strategy.ClientStrategy;
+
 public class Main {
     public static void main(String... args) {
-    	System.out.println("HelloWorld");
+        ClientStrategy client = new ClientStrategy();
+
+        client.run();
     }
 }
