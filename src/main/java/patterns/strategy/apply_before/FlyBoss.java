d9dfd85f32a71d6e341157d77a6858983490966d
kkk <kkk@gmail.com>
2019-03-02 14:06:35 +0900

    설정

--- src/main/java/patterns/strategy/apply_before/FlyBoss.java
+++ src/main/java/patterns/strategy/apply_before/FlyBoss.java
@@ -6,6 +6,10 @@ public class FlyBoss extends Boss {
         this.setName("비행 보스");
     }
 
+    public void move() {
+        System.out.println("비행 보스는 발이 없어서 땅으로 움직일 수 없다.");
+    }
+
     @Override
     public void show() {
         System.out.println("날개는 있지만, 발이 없는 거대한 몬스터");
--- src/main/java/patterns/strategy/apply_before/GroundBoss.java
+++ src/main/java/patterns/strategy/apply_before/GroundBoss.java
@@ -6,6 +6,10 @@ public class GroundBoss extends Boss {
         this.setName("대지 보스");
     }
 
+    public void fly() {
+        System.out.println("대지 보스는 날개가 없어서 날 수 없다.");
+    }
+
     @Override
     public void show() {
         System.out.println("날개 없는 거대한 몬스터");
