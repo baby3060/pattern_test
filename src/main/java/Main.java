20a35759ab8277d27ece35107c28a195bf161476
psk <psk3060@naver.com>
2019-03-05 18:44:30 +0900

    Factory 패턴 관련 설정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,11 @@
  * 각 패턴 패키지의 Client[Pattern명] 클래스를 수행한다.
  */
 
-import patterns.decorator.*;
+import patterns.factory.*;
 
 public class Main {
     public static void main(String... args) {
-        DecoratorClient client = new DecoratorClient();
+        FactoryClient client = new FactoryClient();
 
         client.run();
     }
