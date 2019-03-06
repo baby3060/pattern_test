812d4879547e9c66ea1143ef4cb30985a3749e7d
psk <psk3060@naver.com>
2019-03-06 16:53:58 +0900

    싱글톤 패턴 구현(멀티쓰레드 안전하지 않음)

---
+++ src/main/java/patterns/singleton/Logger.java
@@ -0,0 +1,34 @@
+package patterns.singleton;
+
+/**
+ * 싱글톤은 객체가 무조건 하나임을 의미함.
+ * 싱글톤으로 생각할 만한 클래스는 쓰레드 풀, 커넥션 풀, Factory 패턴에서의 Factory 메소드들, 공통 Logger
+ */
+public class Logger {
+    private Class target;
+    private static Logger logger;
+
+    private Logger() {
+        this(Logger.class);
+    }
+
+    private Logger(Class target) {
+        this.target = target;
+    }
+
+    public static Logger getLogger(Class target) {
+        if( logger == null ) {
+            logger = new Logger(target);
+        }
+        return logger;
+    }
+
+    public void info(String msg) {
+        System.out.println(target.getName() + "[INFO] : " + msg);
+    }
+
+    public void debug(String msg) {
+        System.out.println(target.getName() + "[DEBUG] : " + msg);
+    }
+
+}
