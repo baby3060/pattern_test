9630f0adeec1b181a76297fec2fe60ef8d699abf
psk <psk3060@naver.com>
2019-03-06 16:53:24 +0900

    싱글톤 패턴 테스트용 Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,11 @@
  * 각 패턴 패키지의 Client[Pattern명] 클래스를 수행한다.
  */
 
-import patterns.factory.*;
+import patterns.singleton.*;
 
 public class Main {
     public static void main(String... args) {
-        FactoryClient client = new FactoryClient();
+        SingleClient client = new SingleClient();
 
         client.run();
     }
