5d2a76f88e81af3c5a7d3330aace9fa044d6639b
psk <psk3060@naver.com>
2019-03-06 16:57:17 +0900

    싱글톤 테스트 클라이언트 수정(동등 비교)

--- src/main/java/patterns/singleton/SingleClient.java
+++ src/main/java/patterns/singleton/SingleClient.java
@@ -3,8 +3,11 @@ package patterns.singleton;
 public class SingleClient {
     public void run() {
         Logger logger = Logger.getLogger(SingleClient.class);
+        Logger logger2 = Logger.getLogger(SingleClient.class);
 
         logger.info("Hello Info");
         logger.debug("Hello debug");
+
+        System.out.println(logger == logger2);
     }
 }
