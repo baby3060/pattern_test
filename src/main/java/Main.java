2d07fb6dfc4380ae1a244bfe6461abd249bbb4f3
psk <psk3060@naver.com>
2019-03-07 17:57:12 +0900

    커맨드 패턴 테스트하도록 메인 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,11 @@
  * 각 패턴 패키지의 Client[Pattern명] 클래스를 수행한다.
  */
 
-import patterns.singleton.*;
+import patterns.command.*;
 
 public class Main {
     public static void main(String... args) {
-        SingleClient client = new SingleClient();
+        CommandClient client = new CommandClient();
 
         client.run();
     }
