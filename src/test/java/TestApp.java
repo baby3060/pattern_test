a4d411df3dcc4d866b9dcc0fde95ee91a007595a
psk <psk3060@naver.com>
2019-03-07 17:57:43 +0900

    비어있는 테스트(테스트 내역 있으면 여기다 다 할 생각)

---
+++ src/test/java/TestApp.java
@@ -0,0 +1,12 @@
+import junit.framework.TestCase;
+import patterns.command.*;
+
+public class TestApp extends TestCase {
+    public void testApp() {
+        
+    }
+
+    public void testApp2() {
+        
+    }
+}
