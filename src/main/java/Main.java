d279a908c8bbd2ed47a2b7a41f6a44aa87030d27
psk <psk3060@naver.com>
2019-03-08 19:10:14 +0900

    Main Modify Adapter Pattern Test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,11 @@
  * 각 패턴 패키지의 Client[Pattern명] 클래스를 수행한다.
  */
 
-import patterns.command.*;
+import patterns.adapter.*;
 
 public class Main {
     public static void main(String... args) {
-        CommandClient client = new CommandClient();
+        AdapterClient client = new AdapterClient();
 
         client.run();
     }
