20288f3ce2fe50b9b9e396fed4a6db08d83c7da8
psk <psk3060@naver.com>
2019-03-09 14:17:10 +0900

    LOG 적용

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,20 @@
 
 import patterns.adapter.*;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
     public static void main(String... args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        
+        logger.info("=================Start===================");
+
         AdapterClient client = new AdapterClient();
 
         client.run();
+
+        logger.info("=================End===================");
+        
     }
 }
